1bf85bc10b6eea6d7050b4f7137b6d2c880ad03a
twelveYearsOldCoder <niko.ss@windowslive.com>
2022-10-15 11:28:34 +0300

    basic bootstrap header

--- article.php
+++ article.php
@@ -1,7 +1,13 @@
 <?php
 require_once("config.php");
+
 ob_start();
 session_start();
+
+?>
+    {% include 'templates/header.html.twig' %}
+<?php
+
 $result = getDataFromDB::getSingleArticle($_GET['id']);
 echo $twig->render('article.html.twig',['article'=>$result[0]]);
 
