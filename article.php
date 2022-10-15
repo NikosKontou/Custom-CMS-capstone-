26e76fdecedbad80467fc41351ea70b455a0eaf1
twelveYearsOldCoder <niko.ss@windowslive.com>
2022-10-15 10:56:37 +0300

    properly passing the article object to the article.html.twig

--- article.php
+++ article.php
@@ -3,7 +3,7 @@ require_once("config.php");
 ob_start();
 session_start();
 $result = getDataFromDB::getSingleArticle($_GET['id']);
-echo $twig->render('article.html.twig',['article'=>$result]);
+echo $twig->render('article.html.twig',['article'=>$result[0]]);
 
 
 ?>
