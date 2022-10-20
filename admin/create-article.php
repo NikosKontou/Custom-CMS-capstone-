880831cef00947a4641cceea53a00fafa5cceb22
twelveYearsOldCoder <niko.ss@windowslive.com>
2022-10-20 23:35:55 +0300

    article creation with image, early stage development

--- admin/create-article.php
+++ admin/create-article.php
@@ -16,15 +16,44 @@
 
     $db = DBConnect::setConnection();
 
-    if (isset($_POST['createArticle']) && isset($_POST['title']) && isset($_POST['body'])) {
+    if (isset($_POST['createArticle']) && isset($_POST['title']) && isset($_POST['body'])&& isset($_POST['imageToUpload'])) {
+        $target_dir = "assets/images/";
+        $target_file = $target_dir . basename($_FILES["imageToUpload"]["name"]);
+        $uploadOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo"<script>console.log('exists')</script>";
+            $uploadOk = 0;
+        }
+
+// Check file size
+        if ($_FILES["imageToUpload"]["size"] > 700000) {
+            echo"<script>console.log('size')</script>";
+            $uploadOk = 0;
+        }
+
+// Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "svg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif" ) {
+            echo"<script>console.log('mime')</script>";
+            $uploadOk = 0;
+        }
+        if (move_uploaded_file($_FILES["imageToUpload"]["tmp_name"], $target_file)) {
+            echo"<script>console.log('ok')</script>";
+            echo "The image ". htmlspecialchars( basename( $_FILES["imageToUpload"]["name"])). " has been uploaded.";
+        } else {
+            echo"<script>console.log('generic error')</script>";
+        }
 
         insertDataToDB::createArticle($_POST['title'], $_POST['body']);
     }
 
-//var_dump($result[0]->body);exit;
-//get the same template used to update an article but pass no article data
+    //var_dump($result[0]->body);exit;
+    //get the same template used to update an article but pass no article data
     //phpself is important so that the php file can get the post request from the twig template
-        echo $twig->render('admin/article-edit.html.twig',['phpSelf'=>htmlspecialchars($_SERVER['PHP_SELF'])]);
+    echo $twig->render('admin/article-edit.html.twig',['phpSelf'=>htmlspecialchars($_SERVER['PHP_SELF'])]);
 
 
     ?>
--- lib/dependencies/js.php
+++ lib/dependencies/js.php
@@ -3,5 +3,5 @@
 //bootstrap
 echo('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>');
 //for the header
-echo('<script src="lib/dependencies/js/header.js"></script>');
+echo('<script src="https://u2123125.eu/lib/dependencies/js/header.js"></script>');
 ?>
