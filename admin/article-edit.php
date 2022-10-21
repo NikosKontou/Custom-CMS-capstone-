262ab4d30a8881494486160fe483736835f4e3d5
twelveYearsOldCoder <niko.ss@windowslive.com>
2022-10-21 20:15:11 +0300

    fixed article edit form

--- admin/article-edit.php
+++ admin/article-edit.php
@@ -24,7 +24,7 @@ if (isset($_POST['editArticle']) && isset($_POST['title']) && isset($_POST['body
 //get the article from the DB
 if (isset($_GET['id'])){
     $result = getDataFromDB::getSingleArticle($_GET['id']);
-
+//        var_dump($result);exit();
 //var_dump($result[0]->body);exit;
 //display it in the template
     echo $twig->render('admin/article-edit.html.twig',['article'=>$result[0], 'phpSelf'=>htmlspecialchars($_SERVER['PHP_SELF'])]);
