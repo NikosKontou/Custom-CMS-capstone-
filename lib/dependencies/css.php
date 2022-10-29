77dfd68d706e246cda13ba09302c556165f091a3
twelveYearsOldCoder <niko.ss@windowslive.com>
2022-10-29 12:30:12 +0300

    passing option to footer

--- lib/dependencies/css.php
+++ lib/dependencies/css.php
@@ -2,5 +2,9 @@
 echo('<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">');
 
 echo('<link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">');
+//add the user.css last to overwrite anything else
+echo('<link href="lib/dependencies/css/custom.css" rel="stylesheet">');
 
-    ?>
+
+?>
+
