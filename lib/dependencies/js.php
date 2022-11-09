aaa3a245adc076a798abe18a392734c59e7122e7
twelveYearsOldCoder <niko.ss@windowslive.com>
2022-11-09 13:29:02 +0200

    added defer to scripts

--- lib/dependencies/js.php
+++ lib/dependencies/js.php
@@ -1,7 +1,7 @@
 <!--javascript requirements-->
 <?php
 //bootstrap
-echo('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>');
+echo('<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous" defer></script>');
 //for the header
-echo('<script src="https://u2123125.eu/lib/dependencies/js/header.js"></script>');
+echo('<script src="https://u2123125.eu/lib/dependencies/js/header.js" defer></script>');
 ?>
