06f04f92d6471bc5362fcd4fbda88775dc340ea9
twelveYearsOldCoder <niko.ss@windowslive.com>
2023-02-07 20:55:33 +0200

    actually check the uploaded image and return a bool value

--- lib/dataValidation.php
+++ lib/dataValidation.php
@@ -1,9 +1,10 @@
 <?php
+
 class dataValidation
 {
 //remove special characters
-   public static function rmvSpclChars($string): array|string|null
-   {
+    public static function rmvSpclChars($string): array|string|null
+    {
         $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
 
         return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
@@ -18,38 +19,39 @@ class dataValidation
 
     public static function imageCheck($image): bool
     {
-
+        $res = false;
         $target_dir = "../assets/images/";
         $target_file = $target_dir . basename($image["name"]);
         $uploadOk = 1;
-        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
         // Check if file already exists
         if (file_exists($target_file)) {
-            echo"<script>console.log('exists')</script>";
+            echo "<script>console.log('exists')</script>";
             $uploadOk = 0;
         }
 
 // Check file size
         if ($image["size"] > 700000) {
-            echo"<script>console.log('size')</script>";
+            echo "<script>console.log('size')</script>";
             $uploadOk = 0;
         }
 
 // Allow certain file formats
-        if($imageFileType != "jpg" && $imageFileType != "svg" && $imageFileType != "png" && $imageFileType != "jpeg"
-            && $imageFileType != "gif" ) {
-            echo"<script>console.log('mime')</script>";
+        if ($imageFileType != "jpg" && $imageFileType != "svg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif") {
+            echo "<script>console.log('mime')</script>";
             $uploadOk = 0;
         }
-        if (move_uploaded_file($image["tmp_name"], $target_file)) {
-            echo"<script>console.log('ok')</script>";
-            return true;
-        } else {
-            echo"<script>console.log('generic error')</script>";
-            return false;
+        if ($uploadOk == 1) {
+            if (move_uploaded_file($image["tmp_name"], $target_file)) {
+                echo "<script>console.log('ok')</script>";
+                $res = true;
+            }
         }
 
+        return $res;
     }
 }
+
 ?>
