ef143ca649dcb1727005d8129c3c734f8c3fac6c
twelveYearsOldCoder <99764553+twelveYearsOldCoder@users.noreply.github.com>
2023-02-13 20:25:42 +0200

    Create config.php
    empty template to be edited at every fresh isntall

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+define('LIBRARY', 'lib/');
+define('BASE_URL', '');
+define('DB_VENDOR','mariadb');
+define('DB_HOST','');
+define('DB_NAME','');
+define('DB_USR','');
+define('DB_PWD','');
+require_once ('lib/requirements.php');
+require_once ('templateRequirements.php');
